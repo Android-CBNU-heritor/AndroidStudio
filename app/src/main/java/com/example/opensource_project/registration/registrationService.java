2b36ead3d8069530af16441ac1c06de9f744aff3
jim <takurway@gmail.com>
2021-06-06 04:28:44 +0900

    sign-up code complete

--- app/src/main/java/com/example/opensource_project/registration/registrationService.java
+++ app/src/main/java/com/example/opensource_project/registration/registrationService.java
@@ -20,6 +20,9 @@ import com.example.opensource_project.registration.createDatabaseController;
 
 public class registrationService extends AppCompatActivity {
 
+    // 회원가입 버튼을 동작하게 하는 flag
+    boolean signUpIsOk = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +31,6 @@ public class registrationService extends AppCompatActivity {
         // domain = id, passwd, name, birth... all of these are text type (파일 상 변수이름들)
         // 실제 도메인(유저 입력값) = id, pqsswd, passwdCheck, email address
 
-        // 회원가입 버튼을 동작하게 하는 flag
-        boolean signUpIsOk = false;
 
         // 중복확인 버튼
         Button buttonForDuplication = (Button) findViewById(R.id.checkDuplicate);
@@ -55,22 +56,26 @@ public class registrationService extends AppCompatActivity {
                 cmp = cursor.getString(0); // 에러 유발 코드
 
                 flag = cmp.equals(userId);
-                Log.d(TAG, "첫번쨰 커서가 가리키는 값?: "+ cursor.getString(0));
-                while (cursor.moveToNext()) {
+                Log.d(TAG, "첫번쨰 커서가 가리키는 값?: "+ cmp);
+                do{
+                    cmp = cursor.getString(0);
 
                     Log.d(TAG, "Insert Data: " + userId);
                     Log.d(TAG, "Comparing ID: " + cmp);
 
-                    if (cmp.equals(userId)) {
+                    if(cmp.equals(userId)){
                         Toast.makeText(getApplicationContext(), " 중복된 아이디가 존재합니다.", Toast.LENGTH_LONG).show();
                         flag = true;
+                        signUpIsOk = false; // [*예외처리]: 가능한 아이디 중복 검사 이후 중복된 아이디 입력하고 다시 재검사
                         break;
                     }
+                }while (cursor.moveToNext());// 에러 유발 지점1 - 이미 위에서 첫번째 cursor item의 string을 가지고 왔는데, while 조건문이 처음에
+                // 실행되면서 cursor가 이동한다. 따라서 두번째부터 비교를 시작하고 중복된 아이디를 찾을 수 없음
 
-                    cmp = cursor.getString(0);    // 에러 유발 지점1 - 이미 위에서 첫번째 cursor item의 string을 가지고 왔는데, while 조건문이 처음에
-                                                     // 실행되면서 cursor가 이동한다. 따라서 두번째부터 비교를 시작하고 중복된 아이디를 찾을 수 없음
+                if (!flag){
+                    Toast.makeText(getApplicationContext(), " 사용 가능한 아이디입니다!", Toast.LENGTH_SHORT).show();
+                    signUpIsOk = true;
                 }
-                if (!flag) Toast.makeText(getApplicationContext(), " 사용 가능한 아이디입니다!", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -83,7 +88,8 @@ public class registrationService extends AppCompatActivity {
 
                 // 중복확인을 정상적으로 진행하지 않았을 때, 회원가입 진행을 막음
                 if(signUpIsOk == false){
-                    Toast.makeText(getApplicationContext(), "누락된 부분이 있습니다.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "ID 중복 검사를 실행하세요.", Toast.LENGTH_LONG).show();
+                    return;
                 };
 
                 createDatabaseController mainDB = new createDatabaseController(getApplicationContext(), "Maindb",null, 1);
